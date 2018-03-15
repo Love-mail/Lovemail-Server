7d9dba0edb6aa822ec6bc183de10c51ce26cb774
Trevor <xizhouh@gmail.com>
2018-03-15 02:50:38 -0600

    test: 测试 IP 获取

--- app/controller/test.js
+++ app/controller/test.js
@@ -1,14 +1,13 @@
 'use strict';
 
 const Controller = require('egg').Controller;
-const requestIp = require('request-ip');
 
 class TestController extends Controller {
   async index() {
     const { ctx } = this;
 
     ctx.body = {
-      data: requestIp.getClientIp(ctx.request)
+      data: ctx.ip
     }
   }
 }
