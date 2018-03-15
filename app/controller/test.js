b85d6219f054394bc97d7a5ab5d2e90b12015e37
Trevor <xizhouh@gmail.com>
2018-03-15 02:12:36 -0600

    test: 获取客户端 IP

--- app/controller/test.js
+++ app/controller/test.js
@@ -5,17 +5,9 @@ const Controller = require('egg').Controller;
 class TestController extends Controller {
   async index() {
     const { ctx } = this;
-    let ip = ctx.request.headers['x-forwarded-for'] ||
-    ctx.request.ip ||
-    ctx.request.connection.remoteAddress ||
-    ctx.request.socket.remoteAddress ||
-    ctx.request.connection.socket.remoteAddress || '';
-    if(ip.split(',').length>0){
-      ip = ip.split(',')[0]
-    }
 
     ctx.body = {
-      data: ip
+      data: ctx.host
     }
   }
 }
