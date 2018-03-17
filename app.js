2ee255e913bf6c02bbc0c64b9c9207f0517963bd
Trevor <xizhouh@gmail.com>
2018-03-17 09:41:14 -0600

    fix: 开启启动项目时启动定时器

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 module.exports = app => {
   app.beforeStart(async () => {
     await app.model.sync({ force: false });
-    // await app.runSchedule('update_one');
-    // await app.runSchedule('update_weather');
+    await app.runSchedule('update_one');
+    await app.runSchedule('update_weather');
 
     app.mailgun = require('mailgun-js')({
       apiKey: app.config.mailgun.api_key,
