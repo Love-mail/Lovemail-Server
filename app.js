89c179af6213cdf8e84d457c6c4ae4271f95b2ff
Trevor <xizhouh@gmail.com>
2018-03-19 23:02:55 -0600

    fix: 开启定时器

--- app.js
+++ app.js
@@ -3,9 +3,8 @@
 module.exports = app => {
   app.beforeStart(async () => {
     await app.model.sync({ force: false });
-    await app.runSchedule('send_lovemail');
-    // await app.runSchedule('update_one');
-    // await app.runSchedule('update_weather');
+    await app.runSchedule('update_one');
+    await app.runSchedule('update_weather');
 
     app.mailgun = require('mailgun-js')({
       apiKey: app.config.mailgun.apiKey,
